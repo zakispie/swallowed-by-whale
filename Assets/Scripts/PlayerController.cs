4e9def2b494c5ac26cf6b58898d169bf74b38ed5
zakispie <93385173+zakispie@users.noreply.github.com>
2024-01-24 16:38:09 -0500

    implements deceleration and uses new input system
    also exposes more variables to the editor, and generally includes some nice QoL (such as in-air vs grounded deceleration) and code cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Class for handling player inputs & applying physics to the player
@@ -6,28 +7,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     #region variables
-    [Tooltip("Move Speed")]
-    [SerializeField] private float _moveSpeed;
-    
-    [Tooltip("Jump Strength")]
-    [SerializeField] private float _jumpStrength;
+    [Header("Ground Movement")]
+    [Tooltip("Move Speed")] 
+    [SerializeField] private float moveSpeed;
 
-    public LayerMask groundLayer;
+    [Tooltip("Deceleration Strength")] 
+    [SerializeField] private float deceleration;
+
+    [Header("Air Movement")]
+    [Tooltip("Deceleration in-air Strength (percentage of base deceleration)")] [Range(0f, 1f)] 
+    [SerializeField] private float inAirDecelerationPercent;
+
+    [Tooltip("Number of Jumps (Including First Jump)")] [Min(1)] 
+    [SerializeField] private int numJumps;
+
+    [Tooltip("Jump Strength")] 
+    [SerializeField] private float jumpStrength;
+
+    [Header("Other")]
+    [Tooltip("Ground Layer")]
+    [SerializeField] private LayerMask groundLayer;
 
     // Tracks the player's current movement direction
     private Vector3 _movementDirection;
-    
+
     // Tracks whether the player is currently grounded
     private bool _isGrounded;
 
     // Tracks num jumps player has performed
     private int _jumpCount;
-    
-    // Cache the transform
-    private Transform _transform;
-    
+
+    // Cache the capsule colliders
+    private CapsuleCollider[] _capsuleColliders;
+
     // Cache the rigidbody
     private Rigidbody _rigidbody;
+
+    // Cache the keyboard
+    private Keyboard _keyboard;
+
     #endregion
 
     /// <summary>
@@ -36,7 +54,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _transform = GetComponent<Transform>();
+        _capsuleColliders = GetComponents<CapsuleCollider>();
+        _keyboard = Keyboard.current;
     }
 
     /// <summary>
@@ -44,31 +63,22 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
-
-        Debug.Log("jump count: " + _jumpCount);
-        Debug.Log("Is on ground: " + _isGrounded);
-
-        // Implements only allowing double jump
-        // Checks if player is grounded
-        _isGrounded = IsGrounded();
-        if(_isGrounded){
-            _jumpCount = 0;
-        }
-
-        // TODO refactor to use new InputSystem
-        if (Input.GetKeyDown(KeyCode.W) && (_isGrounded || _jumpCount < 1)) // TODO disable jumping if the player is already in-air
+        // Allow jump if player is grounded or has not performed double jump
+        if (_keyboard.wKey.wasPressedThisFrame && (_isGrounded || _jumpCount < numJumps - 1))
         {
-            _rigidbody.AddForce(Vector3.up * _jumpStrength, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
             _jumpCount++;
         }
-        
-        if (Input.GetKey(KeyCode.A))
+
+        // Handle Inputs
+        if (_keyboard.aKey.isPressed)
         {
             _movementDirection = Vector3.left;
-        } else if (Input.GetKey(KeyCode.D))
+        }
+        else if (_keyboard.dKey.isPressed)
         {
             _movementDirection = Vector3.right;
-        } 
+        }
         else
         {
             _movementDirection = Vector3.zero;
@@ -80,26 +90,57 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        // TODO add deceleration variable and implement it
-        
-        // Initialize movement vector
-        Vector3 movement = _movementDirection * _moveSpeed;
+        // Check if player is grounded
+        _isGrounded = IsGrounded();
+        if (_isGrounded)
+        {
+            _jumpCount = 0;
+        }
 
-        // Apply force
-        _rigidbody.AddForce(movement * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        // Initialize movement vector and apply to the rigidbody
+        Vector3 movement = _movementDirection * (moveSpeed * Time.fixedDeltaTime);
+        
+        if (movement == Vector3.zero) 
+        {
+            // No movement input, we should decelerate
+            Vector3 curVelocity = _rigidbody.velocity;
+            // Apply the deceleration force (on the x-axis only) via a Lerp
+            if (_isGrounded)
+            {
+                _rigidbody.velocity =
+                    Vector3.Lerp(curVelocity, new Vector3(0, curVelocity.y, curVelocity.z),
+                        deceleration * Time.fixedDeltaTime);
+            }
+            else
+            {
+                _rigidbody.velocity =
+                    Vector3.Lerp(curVelocity, new Vector3(0, curVelocity.y, curVelocity.z),
+                        (deceleration * inAirDecelerationPercent) * Time.fixedDeltaTime);
+            }
+        }
+        else
+        {
+            // Movement input requested, apply movement force in that direction
+            _rigidbody.AddForce(movement, ForceMode.VelocityChange);
+        }
     }
 
+    /// <summary>
+    /// Determines if the player is currently grounded
+    /// </summary>
+    /// <returns> True if the player is grounded, false otherwise </returns>
     bool IsGrounded()
     {
         // The position, height, and radius of the capsule's collider.
-        Vector3 capsulePosition = transform.position;
-        float capsuleHeight = GetComponent<CapsuleCollider>().height;
-        float capsuleRadius = GetComponent<CapsuleCollider>().radius;
+        Vector3 capsulePosition = _capsuleColliders[0].transform.position;
+        float capsuleHeight = _capsuleColliders[0].height;
+        float capsuleRadius = _capsuleColliders[0].radius;
 
         // Check for overlapping colliders below the capsule to determine if it's grounded.
-        Collider[] hitColliders = Physics.OverlapCapsule(capsulePosition + new Vector3(0, capsuleHeight / 2 - capsuleRadius, 0),
-                                                          capsulePosition - new Vector3(0, capsuleHeight / 2 - capsuleRadius, 0),
-                                                          capsuleRadius, groundLayer);
+        Collider[] hitColliders = Physics.OverlapCapsule(
+            capsulePosition + new Vector3(0, capsuleHeight / 2 - capsuleRadius, 0),
+            capsulePosition - new Vector3(0, capsuleHeight / 2 - capsuleRadius, 0),
+            capsuleRadius, groundLayer);
 
         return hitColliders.Length > 0;
     }

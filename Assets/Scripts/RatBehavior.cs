b16c953453fd25dad1ce3fa961c54a082fd73ba5
zakispie <93385173+zakispie@users.noreply.github.com>
2024-02-10 20:25:44 -0500

    rats no longer walk off ledges
    it works by raycasting above their head downards at 45 degrees, if the raycast hits something they keep walking, if it hits nothing there is a ledge and they turn around

--- Assets/Scripts/RatBehavior.cs
+++ Assets/Scripts/RatBehavior.cs
@@ -14,12 +14,18 @@ public class RatBehavior : EnemyBehavior
 
     [Tooltip("How many units the rat can actually attack or reach")] [SerializeField]
     private float attackReachDistance;
-    
+
     [Tooltip("How many seconds the rat will wait before attacking")] [SerializeField]
     private float attackTelegraphTime;
-    
+
     [Tooltip("How close to a wall (or other obstacle) will the rat get before turning around?")] [SerializeField]
     private float wallDistance;
+
+    [Tooltip("How many units above the rat should a raycast be cast from to check for a ledge?")] [SerializeField]
+    private float ledgeCheckHeight;
+
+    [Tooltip("How long should the raycast from the ledge check be?")] [SerializeField]
+    private float ledgeCheckDistance;
 
     [Tooltip("Rat move speed")] [SerializeField]
     private float moveSpeed;
@@ -44,11 +50,21 @@ public class RatBehavior : EnemyBehavior
             currentState = BehaviorState.Chase;
             return;
         }
-        
+
         print("Can't see player, wandering...");
 
-        // Check if there's anything in the way of the rat
-        if (Physics.Raycast(transform.position, facingDirection, wallDistance))
+        // Define an origin for start of ledge check
+        var origin = transform.position + (Vector3.up * ledgeCheckHeight);
+        // Define direction to be 45 degrees by making the y of facingDirection = 0.5f
+        var direction = facingDirection + (Vector3.down * 0.5f);
+
+        // Debug to show the ledge check line:
+        // var endpoint = origin + direction * ledgeCheckDistance;
+        // Debug.DrawLine(origin, endpoint);
+
+        // Check if there's anything in the way of the rat, or if the rat is approaching a ledge (raycast for ledge check hits nothing)
+        if (Physics.Raycast(transform.position, facingDirection, wallDistance)
+            || !Physics.Raycast(origin, direction, ledgeCheckDistance))
         {
             facingDirection = facingDirection == Vector3.right ? Vector3.left : Vector3.right;
         }
@@ -61,7 +77,8 @@ public class RatBehavior : EnemyBehavior
     protected override void Chase()
     {
         // check if we have line of sight to the player by raycasting 
-        if (Physics.Raycast(transform.position, PlayerController.Position - transform.position, out var hit, aggroDistance))
+        if (Physics.Raycast(transform.position, PlayerController.Position - transform.position, out var hit,
+                aggroDistance))
         {
             // we hit something
             if (!hit.collider.CompareTag("Player"))
@@ -117,6 +134,7 @@ public class RatBehavior : EnemyBehavior
                 PlayerController.Health.TakeDamage(1);
             }
         }
+
         attacking = false;
         currentState = BehaviorState.Wander;
     }
@@ -130,5 +148,4 @@ public class RatBehavior : EnemyBehavior
         return Physics.Raycast(transform.position, facingDirection, out var hit, aggroDistance) &&
                hit.collider.CompareTag("Player");
     }
-
 }

ab853f04694941f0151c97804a9209a0d33ecf7b
gus-s-42 <gusnbastar@gmail.com>
2024-03-11 19:03:31 -0400

    Updated E to Interact to work on trigger

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,32 +6,31 @@ using TMPro;
 
 public class Interactable : MonoBehaviour
 {
-    [Tooltip("Player Reference")]
-    [SerializeField] private GameObject playerReference;
-
     [Tooltip("E To Interact Text Reference")]
     [SerializeField] private TMP_Text UITextReference;
-
-    [Tooltip("Distance Until Iteractable")]
-    [SerializeField] private float _distanceUntilInteractable = 4.0f;
-
-    private float _distance;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        _distance = 0f;
+        UITextReference.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _distance = Vector3.Distance(this.transform.position, playerReference.transform.position);
-        if(_distance <= _distanceUntilInteractable)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
         {
             UITextReference.enabled = true;
-        } else
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
         {
             UITextReference.enabled = false;
         }

d5962b773d0d860516ff6ad139b3854eb1e093f9
gus-s-42 <gusnbastar@gmail.com>
2024-03-21 21:12:15 -0400

    Added more transitions for the player animations and cleaned up the player animation script.

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -40,34 +40,16 @@ public class PlayerAnimation : MonoBehaviour
         if (FacingDirection.Equals(Vector3.up))
         {
             //Debug.Log("Facing Direction: up");
-            AnimationController.SetBool("Idle", false);
-            AnimationController.SetBool("Run", false);
-            AnimationController.SetBool("Crouch", false);
-            AnimationController.SetBool("Jump", true);
-            AnimationController.SetBool("CrouchWalk", false);
-            AnimationController.SetBool("Climb", false);
-            AnimationController.SetBool("ClimbingIdle", false);
+            SetAnimation("Jump");
         }
         else if (MovementDirection.Equals(Vector3.zero) && !OnLadder)
         {
-            AnimationController.SetBool("Idle", true);
-            AnimationController.SetBool("Run", false);
-            AnimationController.SetBool("Crouch", false);
-            AnimationController.SetBool("Jump", false);
-            AnimationController.SetBool("CrouchWalk", false);
-            AnimationController.SetBool("Climb", false);
-            AnimationController.SetBool("ClimbingIdle", false);
+            SetAnimation("Idle");
         }
         else if (FacingDirection.Equals(Vector3.left) || FacingDirection.Equals(Vector3.right))
         {
-        //    Debug.Log("Facing Direction: left or right");
-            AnimationController.SetBool("Idle", false);
-            AnimationController.SetBool("Run", true);
-            AnimationController.SetBool("Crouch", false);
-            AnimationController.SetBool("Jump", false);
-            AnimationController.SetBool("CrouchWalk", false);
-            AnimationController.SetBool("Climb", false);
-            AnimationController.SetBool("ClimbingIdle", false);
+            //    Debug.Log("Facing Direction: left or right");
+            SetAnimation("Run");
         }
 
         if (IsCrouched)
@@ -75,47 +57,70 @@ public class PlayerAnimation : MonoBehaviour
         //    Debug.Log("Facing Direction: down");
             if(MovementDirection.Equals(Vector3.zero))
             {
-                AnimationController.SetBool("Idle", false);
-                AnimationController.SetBool("Run", false);
-                AnimationController.SetBool("Crouch", true);
-                AnimationController.SetBool("Jump", false);
-                AnimationController.SetBool("CrouchWalk", false);
-                AnimationController.SetBool("Climb", false);
-                AnimationController.SetBool("ClimbingIdle", false);
+                SetAnimation("Crouch");
             } else
             {
-                AnimationController.SetBool("Idle", false);
-                AnimationController.SetBool("Run", false);
-                AnimationController.SetBool("Crouch", false);
-                AnimationController.SetBool("Jump", false);
-                AnimationController.SetBool("CrouchWalk", true);
-                AnimationController.SetBool("Climb", false);
-                AnimationController.SetBool("ClimbingIdle", false);
+                SetAnimation("CrouchWalk");
             }
         }
 
         if(OnLadder)
         {
+            Debug.Log("We should be on the ladder.");
             if(Keyboard.wKey.isPressed || Keyboard.sKey.isPressed)
             {
-                AnimationController.SetBool("Idle", false);
-                AnimationController.SetBool("Run", false);
-                AnimationController.SetBool("Crouch", false);
-                AnimationController.SetBool("Jump", false);
-                AnimationController.SetBool("CrouchWalk", false);
-                AnimationController.SetBool("Climb", true);
-                AnimationController.SetBool("ClimbingIdle", false);
+                SetAnimation("Climb");
             } else if (!Keyboard.wKey.isPressed && !Keyboard.sKey.isPressed)
             {
                 //if not moving on ladder, pause animation
-                AnimationController.SetBool("Idle", false);
-                AnimationController.SetBool("Run", false);
-                AnimationController.SetBool("Crouch", false);
-                AnimationController.SetBool("Jump", false);
-                AnimationController.SetBool("CrouchWalk", false);
-                AnimationController.SetBool("Climb", false);
-                AnimationController.SetBool("ClimbingIdle", true);
+                SetAnimation("ClimbingIdle");
             }
+        }
+
+        Debug.Log("Idle: " + AnimationController.GetBool("Idle")
+            + "\nRun: " + AnimationController.GetBool("Run")
+            + "\nCrouch: " + AnimationController.GetBool("Crouch")
+            + "\nJump: " + AnimationController.GetBool("Jump")
+            + "\nCrouchWalk: " + AnimationController.GetBool("CrouchWalk")
+            + "\nClimb: " + AnimationController.GetBool("Climb")
+            + "\nClimbingIdle: " + AnimationController.GetBool("ClimbingIdle"));
+    }
+
+
+    //Takes in a string that will determine which animation to play
+    void SetAnimation(string animationTitle)
+    {
+        AnimationController.SetBool("Idle", false);
+        AnimationController.SetBool("Run", false);
+        AnimationController.SetBool("Crouch", false);
+        AnimationController.SetBool("Jump", false);
+        AnimationController.SetBool("CrouchWalk", false);
+        AnimationController.SetBool("Climb", false);
+        AnimationController.SetBool("ClimbingIdle", false);
+
+        switch (animationTitle)
+        {
+            case "Idle":
+                AnimationController.SetBool("Idle", true);
+                break;
+            case "Run":
+                AnimationController.SetBool("Run", true);
+                break;
+            case "Crouch":
+                AnimationController.SetBool("Crouch", true);
+                break;
+            case "Jump":
+                AnimationController.SetBool("Jump", true);
+                break;
+            case "CrouchWalk":
+                AnimationController.SetBool("CrouchWalk", true);
+                break;
+            case "Climb":
+                AnimationController.SetBool("Climb", true);
+                break;
+            case "ClimbingIdle":
+                AnimationController.SetBool("ClimbingIdle", true);
+                break;
         }
     }
 }

483cf4630a54fe0bf12fcbc68ebc0b872bfc6963
Rae Albus <116688356+RaeAlbus@users.noreply.github.com>
2024-04-08 18:09:26 -0400

    Added Dialouge

---
+++ Assets/Scripts/AgnesDialouge.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Linq;
+
+public class AgnesDialouge : MonoBehaviour
+{
+
+    public List<string> foundDialouge;
+    public List<string> hasKeyDialouge;
+    public List<string> hasNoKeyDialouge;
+    public List<string> stillLookingDialouge;
+    public List<string> foundKeyDialouge;
+
+    private bool hasMet;
+    private bool inConvo;
+
+    void Start()
+    {
+        hasMet = false;
+        inConvo = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player") && !inConvo)
+        {
+            List<string> dialouge = new List<string>();
+            inConvo = true;
+
+            if(!hasMet)
+            {
+
+                if(PlayerController._hasKey)
+                {
+                    dialouge = foundDialouge.Concat(hasKeyDialouge).ToList();
+                }
+                else
+                {
+                    dialouge = foundDialouge.Concat(hasNoKeyDialouge).ToList();
+                }
+
+            }
+            else if (hasMet)
+            {
+
+                if(PlayerController._hasKey)
+                {
+                    dialouge = foundKeyDialouge;
+                }
+                else
+                {
+                    dialouge = stillLookingDialouge;
+                }
+
+            }
+
+            SoundManager.Instance.InitDialouge(dialouge.ToArray());
+            hasMet = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (inConvo)
+        {
+            inConvo = false;
+        }
+    }
+}
